c168277baf50ace7c6242089b2d2765a636f1f4a
GT <FTowerT@outlook.com>
2021-09-27 21:41:46 +0300

    Bug fixes

--- src/components/TempBody/index.tsx
+++ src/components/TempBody/index.tsx
@@ -11,12 +11,12 @@ export default function TempBody () {
   return (
     <div className={ Styles.mainCont }>
       <Block title={"Сучасна команда зубних техніків"} image={Team}>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      Hac maecenas feugiat mauris, lacus non urna facilisis mattis.
-      Quam a, posuere ut convallis. Neque aliquam lacus tempor sed sed cum nec.
-      Eu morbi sit quis semper quam. Molestie quis lobortis quam massa
-      dictum facilisi mauris venenatis. Ut nisl enim mauris odio pulvinar
-      dignissim porta phasellus. Praesent et egestas at gravida gravida ultricies quis non.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Hac maecenas feugiat mauris, lacus non urna facilisis mattis.
+        Quam a, posuere ut convallis. Neque aliquam lacus tempor sed sed cum nec.
+        Eu morbi sit quis semper quam. Molestie quis lobortis quam massa
+        dictum facilisi mauris venenatis. Ut nisl enim mauris odio pulvinar
+        dignissim porta phasellus. Praesent et egestas at gravida gravida ultricies quis non.
       </Block>
     </div>
   );
